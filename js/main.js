15711bb49dfd5f4c071d12ea888a1829e517e41f
abdallah1026 <abdallahahmed0600@gmail.com>
2023-07-31 18:51:00 +0300

    create new project

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let x = document.querySelector('.imges');
+let y = document.querySelector('.container');
+let u = document.querySelector('#menu');
+function photos(phone){
+    x.src =phone;
+}
+function color(c1){
+    y.style.background = c1
+
+}
